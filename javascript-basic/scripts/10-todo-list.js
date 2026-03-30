2a35a19200cbe3f0c4e1c7ca07c82ebf0527fb43
KasuniMadhara <kasuniwijerathna2003@gmail.com>
2026-03-30 20:00:38 +0530

    todo list with loops

--- javascript-basic/scripts/10-todo-list.js
+++ javascript-basic/scripts/10-todo-list.js
@@ -1,4 +1,23 @@
-const todoList = []; //empty array
+const todoList = ['make dinner', 'watch you tube', 'eat ontime']; //empty array
+
+renderTodoList();
+
+function renderTodoList () {
+        let todoListHTML = '';
+
+
+    for (let i = 0; i < todoList.length; i ++) {
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+    };
+
+    console.log(todoListHTML);
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+}
+
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
@@ -8,4 +27,6 @@ function addTodo() {
     console.log(todoList); //log the array to the console
 
     inputElement.value = ''; //clear the input field after adding the item
+
+    renderTodoList();
 }
