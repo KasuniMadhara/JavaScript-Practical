a946fa00c0bb534bc758962c2f534b03c6cd1606
KasuniMadhara <kasuniwijerathna2003@gmail.com>
2026-03-31 19:59:30 +0530

    Make delete button using JS

---
+++ javascript-basic/scripts/10-todo-list2.js
@@ -0,0 +1,39 @@
+const todoList = ['make dinner', 'watch you tube', 'eat ontime']; //empty array
+
+renderTodoList();
+
+function renderTodoList () {
+        let todoListHTML = '';
+
+
+    for (let i = 0; i < todoList.length; i ++) {
+        const todo = todoList[i];
+
+        const html = 
+        `<p>${todo}
+        <button onclick="
+          todoList.splice(${i},1); //splice method is used to remove an item from the array  
+          renderTodoList(); //re-render the list after deletion
+        ">DELETE</button>
+        </p>`;
+    todoListHTML += html;
+    };
+
+    console.log(todoListHTML);
+
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+}
+
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value; //todo item is stored in the variable name
+    
+    todoList.push(name); //name(values) is added to the end of the array
+    console.log(todoList); //log the array to the console
+
+    inputElement.value = ''; //clear the input field after adding the item
+
+    renderTodoList();
+}
